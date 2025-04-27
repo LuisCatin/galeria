47d324392d81b5746cdc2c28f83b1694ab270c1e
Luis Manzano <catinlamh@gmail.com>
2025-04-27 00:25:32 -0500

    remove Quasar logo and simplify IndexPage layout

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,13 +1,14 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+    <q-btn color="green" unelevated> luis </q-btn>
+    <div class="luis">luis</div>
   </q-page>
 </template>
 
-<script setup>
-//
-</script>
+<script setup></script>
+
+<style scoped lang="scss">
+.luis {
+  color: red;
+}
+</style>
