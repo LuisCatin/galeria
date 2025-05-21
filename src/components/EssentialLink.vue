365c726dd7dbe48ec383c9b4fbc0f83c413c3431
Karel Diaz <kdiazalfonso94@gmail.com>
2025-05-21 13:10:03 -0500

    feat: update EssentialLink component to use router-link and modify MainLayout menu header

--- src/components/EssentialLink.vue
+++ src/components/EssentialLink.vue
@@ -1,16 +1,3 @@
-<template>
-  <q-item clickable tag="a" :href="props.link">
-    <q-item-section v-if="props.icon" avatar>
-      <q-icon :name="props.icon" />
-    </q-item-section>
-
-    <q-item-section>
-      <q-item-label>{{ props.title }}</q-item-label>
-      <q-item-label caption>{{ props.caption }}</q-item-label>
-    </q-item-section>
-  </q-item>
-</template>
-
 <script setup>
 const props = defineProps({
   title: {
@@ -34,3 +21,25 @@ const props = defineProps({
   },
 })
 </script>
+
+<template>
+  <router-link :to="props.link" class="link">
+    <q-item clickable>
+      <q-item-section v-if="props.icon" avatar>
+        <q-icon :name="props.icon" />
+      </q-item-section>
+
+      <q-item-section>
+        <q-item-label>{{ props.title }}</q-item-label>
+        <q-item-label caption>{{ props.caption }}</q-item-label>
+      </q-item-section>
+    </q-item>
+  </router-link>
+</template>
+
+<style lang="scss" scoped>
+.link {
+  text-decoration: none;
+  color: black;
+}
+</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -5,14 +5,12 @@
         <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
 
         <q-toolbar-title> Galeria </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
       </q-toolbar>
     </q-header>
 
     <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label header> Essential Links </q-item-label>
+        <q-item-label header> Menu </q-item-label>
 
         <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
